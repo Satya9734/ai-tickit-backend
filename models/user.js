bae82dd12ab4abadb6e458e69289e5a6732b31ea
satya mallick <satyamallick973@gmail.com>
2026-01-11 22:58:06 +0530

    fixed user roles

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const userSchema= new mongoose.Schema({
     email:{type:String, required:true, unique:true},
     name:{type:String, required:true, unique:true},
     password:{type:String, required:true},
-    role:{type:String, default:"user", enum:["user","admin","moderetor"]},
+    role:{type:String, default:"user", enum:["user","admin","moderator"]},
     skills:[String],
     createAt:{default:Date.now , type:Date}
 })
