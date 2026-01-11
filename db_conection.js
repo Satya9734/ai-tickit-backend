2f58c5fa6a0900e438511688c0b78e93276f1a5b
satya mallick <satyamallick973@gmail.com>
2026-01-11 19:21:23 +0530

    edit db conection

--- db_conection.js
+++ db_conection.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const db_conection=async()=>{
 try {
-    const db_conection=await mongoose.connect(`mongodb+srv://${process.env.name}:${process/env.password}@cluster0.8rkic.mongodb.net/AItickitDB?retryWrites=true&w=majority`);
+    const db_conection=
+    await mongoose.connect(`mongodb+srv://${process.env.name}:${process.env.password}@cluster0.8rkic.mongodb.net/AItickitDB?retryWrites=true&w=majority`);
     console.log("database conected");
 } catch (error) {
     console.log("error in database conection : ",error)
